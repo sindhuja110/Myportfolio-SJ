2b77dacaa24b689eb803399428b93d73446ff18f
sindhuja110 <sindhuja1106@gmail.com>
2024-01-21 20:58:29 +0530

    made some ui improvement for contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import { Col, Container, Form,  Row,  Toast } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Col, Container, Form, Row, Toast } from "react-bootstrap";
 import { LuSend } from "react-icons/lu";
-import { MdContacts, MdLocationOn } from "react-icons/md";
+import { MdContacts, MdLocationOn, MdOutlineMailOutline } from "react-icons/md";
 import { MdEmail } from "react-icons/md";
-import BasicButton from '../components/BasicButton';
-import emailjs from '@emailjs/browser';
+import BasicButton from "../components/BasicButton";
+import emailjs from "@emailjs/browser";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 const ContactMe = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
 
   const [showToast, setShowToast] = useState(false);
@@ -22,7 +24,6 @@ const ContactMe = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    
     try {
       const templateParams = {
         user_name: formData.name,
@@ -31,138 +32,221 @@ const ContactMe = () => {
       };
 
       // Replace 'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', and 'YOUR_USER_ID' with your actual EmailJS IDs
-      await emailjs.send('service_3crvd62', 'template_2pqpvxf', templateParams, 'e1xtCQtBW0GAeExFG');
+      await emailjs.send(
+        "service_3crvd62",
+        "template_2pqpvxf",
+        templateParams,
+        "e1xtCQtBW0GAeExFG"
+      );
 
       // Show the success toast
       setShowToast(true);
 
       // Clear the form data after submission
       setFormData({
-        name: '',
-        email: '',
-        message: '',
+        name: "",
+        email: "",
+        message: "",
       });
     } catch (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
       // Handle error (e.g., show an error toast)
     }
   };
 
   return (
-    <Container fluid className='text-light mt-5'>
-      <div className='text-center' style={{marginBottom:"50px"}}>
-        <h3 id='cnt' className='bg-clr mx-2' style={{ fontFamily: 'serif', marginBottom:"10px" }}> <MdContacts style={{marginRight:"10px"}} />Contact Me</h3>
-        <p>Feel free to reach out to me using the form below.  I'll get back to you as soon as possible!</p>
+    <Container fluid className="text-light mt-5">
+      <div className="text-center" style={{ marginBottom: "50px" }}>
+        <h3
+          id="cnt"
+          className="bg-clr mx-2"
+          style={{ fontFamily: "serif", marginBottom: "10px" }}
+        >
+          {" "}
+          <MdContacts style={{ marginRight: "10px" }} />
+          Contact Me
+        </h3>
+        <p>
+          Feel free to reach out to me using the form below. I'll get back to
+          you as soon as possible!
+        </p>
       </div>
-      <Row className='d-flex flex-row justfy-content-start align-items-center'>
- <Col className='d-flex flex-column align-items-center justfy-content-center text-center m-3'>
-      <div >
-        <iframe
-          title="Google Maps"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.8755638933294!2d79.82116893759436!3d10.972764044631088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a551749493af835%3A0x93f7674752d2b8b!2sRayanpalayam%20Main%20Rd%2C%20Puducherry%20609609!5e0!3m2!1sen!2sin!4v1704109146897!5m2!1sen!2sin"
-          width="270"
-          height="180"
-          style={{ border: "ActiveBorder", borderRadius: '10px' }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
+      <Row className="d-flex flex-row justfy-content-start align-items-center">
+        <Col className="d-flex flex-column align-items-center justfy-content-center text-center m-3">
+          <div>
+            <iframe
+              title="Google Maps"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d979.2186881746637!2d79.82446934302189!3d10.972825245555098!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5517cc9af6a0fb%3A0x89d50e53eccc9d13!2sRayanpalayam%20bus%20stop!5e0!3m2!1sen!2sin!4v1705841481017!5m2!1sen!2sin"
+              width="270"
+              height="180"
+              style={{ border: "ActiveBorder", borderRadius: "10px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
+
+          <address
+            className="mt-3"
+            style={{
+              fontSize: "17px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <MdLocationOn size={35} style={{ color: "coral" }} />
+            <span>
+              27, Mathina Street, Rayanpalaiyam,
+              <br />
+              Karaikal-609609.
+            </span>
+          </address>
+
+          <a
+            href="https://www.google.com/gmail/about/"
+            style={{
+              textDecoration: "none",
+              color: "white",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginTop: "10px",
+            }}
+          >
+            <MdEmail size={30} style={{ color: "coral" }} />
+            sindhuja1106@gmail.com
+          </a>
+        </Col>
+        <Col className="m-3">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name" className="mt-3">
+              <Form.Label> Name :</Form.Label>
+              <Form.Control
+          type="text"
+          placeholder="Your Name.."
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          style={{
+            border: '4px solid coral',
+            transition: 'border-color 0.3s',
+          }}
+          required
         />
-      </div>
-
-      <address className='mt-3' style={{ fontSize: "17px", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <MdLocationOn size={35} style={{ color: 'coral' }} />
-        <span>
-          27, Mathina Street, Rayanpalaiyam,<br />
-          Karaikal-609609.
-        </span>
-      </address>
-
-      <a href='https://www.google.com/gmail/about/' style={{ textDecoration: "none", color: "white", display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10px' }}>
-        <MdEmail size={30} style={{ color: 'coral' }} />
-        sindhuja1106@gmail.com
-      </a>
-    </Col>
-      <Col className='m-3'>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="name" className='mt-3'>
-          <Form.Label> Name :</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder='Enter your Name..'
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
         </Form.Group>
+            <Form.Group controlId="email" className="mt-3">
+              <Form.Label>Email :</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Your Email.."
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                style={{
+                  border: '4px solid coral',
+                  transition: 'border-color 0.3s',
+                }}
+                required
+              />
+            </Form.Group>
 
-        <Form.Group controlId="email" className='mt-3'>
-          <Form.Label>Email :</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder='Enter your Email..'
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        
+            <Form.Group controlId="message" className="mt-3">
+              <Form.Label>Message :</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Your Message.."
+                rows={4}
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                style={{
+                  border: '4px solid coral',
+                  transition: 'border-color 0.3s',
+                }}
+                required
+              />
+            </Form.Group>
+            <Col className="text-center">
+              <BasicButton
+                label="Send Message"
+                icon={<LuSend size={27} />}
+                color="coral"
+                type="submit"
+                tcolor="black"
+                className="mt-4 text-center boxsd"
+                onClick={handleSubmit}
+              />
+            </Col>
+          </Form>
 
-        <Form.Group controlId="message" className='mt-3'>
-          <Form.Label>Message :</Form.Label>
-          <Form.Control
-            as="textarea"
-            placeholder='Enter your Message..'
-            rows={4}
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-<Col className='text-center'>
-        <BasicButton 
-        label="Send Message"
-         icon={<LuSend size={27}/>} 
-         color="coral" 
-         type="submit" 
-         tcolor="black"
-         className='mt-4 text-center boxsd'
-         onClick={ handleSubmit}
-            
-         />
-          </Col>
-      </Form>
-
-      <Toast
-        show={showToast}
-        onClose={() => setShowToast(false)}
-        autohide
-        delay={2500}
-        style={{
-          position: 'fixed',
-          bottom: 20,
-          right: 20,
-          backgroundColor: 'black',
-          minWidth: '200px',
-          maxWidth: '70%',
-        }}
-      >
-        <Toast.Header className='bgc-clr'>
-          <strong className="mr-auto">Thanks You..!</strong>
-        </Toast.Header>
-        <Toast.Body className='text-light'>Your message has been sent successfully</Toast.Body>
-      </Toast>
-      </Col>
+          <Toast
+            show={showToast}
+            onClose={() => setShowToast(false)}
+            autohide
+            delay={2500}
+            style={{
+              position: "fixed",
+              bottom: 20,
+              right: 20,
+              backgroundColor: "black",
+              minWidth: "200px",
+              maxWidth: "70%",
+            }}
+          >
+            <Toast.Header className="bgc-clr">
+              <strong className="mr-auto">Thanks You..!</strong>
+            </Toast.Header>
+            <Toast.Body className="text-light">
+              Your message has been sent successfully
+            </Toast.Body>
+          </Toast>
+        </Col>
       </Row>
-      <footer className="bg-dark text-light py-1 mt-4" >
+      <footer className="bg-dark text-light py-1 mt-4">
         <hr className="my-2" />
-          <Col className="text-center text-noWrap">
-            <p> Copyrights &copy; 2024 sindhuja_ak.<br/>
-              All rights reserved.</p>
+        <Col className="text-center text-noWrap">
+          <p>
+            {" "}
+            Copyrights &copy; 2024 sindhuja_ak.
+            <br />
+            All rights reserved.
+          </p>
+        </Col>
+        <Col className="bg-clr text-center">
+            {/* Other social media links */}
+          <a
+              href="https://www.instagram.com/sindhuja_ak?igsh=ODA1NTc5OTg5Nw=="
+              target="blank"
+              className="bg-clr"
+            >
+              <FaInstagram size={27} className="m-3 icon" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sindhuja-ak-a72a49282?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+              target="blank"
+              className="bg-clr"
+            >
+              <FaLinkedin size={27} className="m-3 icon" />
+            </a>
+            <a
+              href="https://github.com/sindhuja110"
+              target="blank"
+              className="bg-clr "
+            >
+              <FaGithub size={27} className="m-3 icon" />
+            </a>
+
+            {/* Mail icon link */}
+            <a
+              href="https://www.google.com/gmail/about/"
+              target="blank"
+              className="bg-clr "
+            >
+              <MdOutlineMailOutline size={27} className="m-3 icon" />
+            </a>
           </Col>
-    </footer>
+      </footer>
     </Container>
   );
 };
