1a04f7c09c60f977fa23c5619a9d41f94ac86685
sindhuja110 <sindhuja1106@gmail.com>
2024-01-21 20:58:44 +0530

    create a perfect skill page for portfolio

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { LiaLaptopCodeSolid } from "react-icons/lia";
+import { Col, Container, Row } from 'react-bootstrap';
+import { LiaLaptopCodeSolid } from 'react-icons/lia';
+import SkillsCard from '../components/SkillCard';
+import htmlImage from '../Assets/html.png';
+import cssImage from '../Assets/css.png';
+import jsImage from '../Assets/js.png';
+import reactImage from '../Assets/react.png';
+import fluImage from '../Assets/fltr.png';
+import btsImage from '../Assets/btsp.png';
+import gitImage from '../Assets/git.png';
+import githImage from '../Assets/github.png';
+import vsImage from '../Assets/vscd.png';
+import pstImage from '../Assets/postman.png';
 
 const Skills = () => {
-    return(
-        
-        <Container fluid className='mt-5 '>
-      <h3 id="skl" className="bg-clr d-flex justify-content-center align-items-end" style={{ fontFamily: 'serif', marginBottom: "10px" }}>
-         <LiaLaptopCodeSolid size={32} style={{ marginRight:"10px" }} /> SKILLS
+  return (
+    <Container fluid className="mt-5">
+      <h3
+        id="skl"
+        className="bg-clr d-flex justify-content-center align-items-end"
+        style={{ fontFamily: 'serif', marginBottom: '10px' }}
+      >
+        <LiaLaptopCodeSolid size={32} style={{ marginRight: '10px' }} /> SKILLS
       </h3>
-      </Container>
-      )
-}
-export default Skills
+      <Row className="text-light">
+        <Col xs={12} className="text-center text-light">
+          <h3>Programming Languages</h3>
+          <h6 className='bg-clr'>I Known</h6>
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="HTML" imageUrl={htmlImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="CSS" imageUrl={cssImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="React" imageUrl={btsImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="JS" imageUrl={jsImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="React" imageUrl={reactImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="Flutter" imageUrl={fluImage} borderColor="coral" />
+        </Col>
+      </Row>
+      <Row className="text-light justify-content-center">
+        <Col xs={12} className="text-center text-light">
+          <h3>Tools</h3>
+          <h6 className='bg-clr'>I use</h6>
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="VS Code" imageUrl={vsImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="Git" imageUrl={gitImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="GitHub" imageUrl={githImage} borderColor="coral" />
+        </Col>
+        <Col xs={6} md={4} lg={2}>
+          <SkillsCard title="Postman" imageUrl={pstImage} borderColor="coral" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Skills;
