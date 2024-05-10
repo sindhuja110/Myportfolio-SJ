81e54455e4651be1c56b9d2b05f16983e88b8fd5
sindhuja110 <sindhuja1106@gmail.com>
2024-05-10 19:55:11 +0530

    change the button style

--- src/components/Togglebutton.js
+++ src/components/Togglebutton.js
@@ -1,32 +1,27 @@
 // DarkLightModeToggle.js
 import React from 'react';
-import { Form } from 'react-bootstrap';
 import { MdLightMode, MdOutlineLightMode } from 'react-icons/md';
 import { useTheme } from './Themecontext';
+import { Form } from 'react-bootstrap';
 
 const ToggleButton = () => {
   const { isDarkMode, toggleTheme } = useTheme();
+
   const handleModeToggle = () => {
     toggleTheme();
   };
+
   return (
     <Form>
-      <Form.Check
-        type="switch"
-        id="darkModeSwitch"
-        label={isDarkMode ? <MdLightMode size={20} /> : <MdOutlineLightMode size={20} />}
-        checked={isDarkMode}
-        onChange={handleModeToggle}
-        className='mt-2'
-      />
+      <div
+        className="mt-2"
+        onClick={handleModeToggle}
+        style={{ cursor: 'pointer' }}
+      >
+        {isDarkMode ? <MdLightMode size={25} /> : <MdOutlineLightMode size={25} />}
+      </div>
     </Form>
   );
 };
+
 export default ToggleButton;
-
-
-
-
-
-
-
